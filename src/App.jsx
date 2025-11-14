d88c3429d1f037aac0fad3baa83fdea6fac7a4d5
Dhyey <dsitapara@gmail.com>
2025-11-14 15:59:20 +0530

    Integrated Gemini API key

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,62 @@
+import { useState } from "react";
+import { URL } from "./constants";
+
+
 function App(){
+
+    const[question,setQuestion] = useState('')
+    const[result,setResult] = useState(undefined)
+
+   const payload = {
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": question
+          }
+        ]
+      }
+    ]
+  }
+  
+
+    const askQuestion = async() => {
+
+      let response = await fetch(URL, {
+        method:  "POST",
+        body : JSON.stringify(payload)
+      })
+      
+    
+
+
+   response = await response.json();
+    setResult(response.candidates[0].content.parts[0].text)
+    }
+
   return(
-    <div className="grid grid-cols-5 h-screen text-center">
+    <div className="grid grid-cols-5 h-screen text-center overflow-hidden">
 
       <div className = "col-span-1 bg-gray-900">
 
       </div>
 
 
-    <div className="col-span-4">
+    <div className="col-span-4 p-10">
 
-      <div className="container h-160" ></div>
+<div className=" w-full container  overflow-x-hidden h-150 overflow-y-auto">
+
+      <div className="text-white ">
+        {result}
+      </div>
+
+
+
+      </div>
 
       <div className="bg-gray-900 w-1/2 p-5 pr-5 text-white border border-blue-700 rounded-lg m-auto flex" >
-        <input className="w-full h-full p-2 outline-none" type="text" placeholder="Ask me anything" />
-        <button className="px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-800 transition">Enter</button>
+        <input className="w-full h-full p-2 outline-none" value = {question} onChange={(event)=> setQuestion(event.target.value)} type="text" placeholder="Ask me anything" />
+        <button className="px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-800 transition" onClick={askQuestion } >Enter</button>
       </div>
 
     </div>
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDCI5uMCBp7ze0brpkIG6LuvoDjm5KhsCo"
+ 
