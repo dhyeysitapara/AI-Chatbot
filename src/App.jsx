fdf94f7bb784aa03d74fafcaac894100d074bf21
Dhyey <dsitapara@gmail.com>
2025-11-15 18:22:16 +0530

    Handling the data from Gemini API

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,73 @@
 import { useState } from "react";
 import { URL } from "./constants";
+import Answers from "./components/Answers";
 
+function App() {
+  const [question, setQuestion] = useState("");
+  const [result, setResult] = useState(undefined);
 
-function App(){
-
-    const[question,setQuestion] = useState('')
-    const[result,setResult] = useState(undefined)
-
-   const payload = {
-    "contents": [
+  const payload = {
+    contents: [
       {
-        "parts": [
+        parts: [
           {
-            "text": question
-          }
-        ]
-      }
-    ]
-  }
-  
+            text: question,
+          },
+        ],
+      },
+    ],
+  };
 
-    const askQuestion = async() => {
+  const askQuestion = async () => {
+    let response = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(payload),
+    });
 
-      let response = await fetch(URL, {
-        method:  "POST",
-        body : JSON.stringify(payload)
-      })
-      
-    
+    response = await response.json();
 
+    let dataString = response.candidates[0].content.parts[0].text;
 
-   response = await response.json();
-    setResult(response.candidates[0].content.parts[0].text)
-    }
+    dataString = dataString.split("* ");
+    dataString = dataString.map((item) => item.trim());
 
-  return(
+    setResult(dataString);
+  };
+
+  return (
     <div className="grid grid-cols-5 h-screen text-center overflow-hidden">
+      <div className="col-span-1 bg-gray-900"></div>
+<div className="col-span-4 p-10">
+  <div className="w-full container overflow-x-hidden h-150 overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-gray-700 [&::-webkit-scrollbar-thumb]:rounded-full">
+    <div className="text-white">
+            <ul>
+            {/* {result} */}
 
-      <div className = "col-span-1 bg-gray-900">
+            {result && result.map((item,index) =>
+              <li className="text-left p-1"> <Answers ans = {item} /></li> )}
 
+            </ul>
+          </div>
+        </div>
+
+        <div className="bg-gray-900 w-1/2 p-5 pr-5 text-white border border-blue-700 rounded-lg m-auto flex">
+          <input
+            className="w-full h-full p-2 outline-none"
+            value={question}
+            onChange={(event) => setQuestion(event.target.value)}
+            type="text"
+            placeholder="Ask me anything"
+          />
+          <button
+            className="px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-800 transition"
+            onClick={askQuestion}
+          >
+            Enter
+          </button>
+        </div>
       </div>
-
-
-    <div className="col-span-4 p-10">
-
-<div className=" w-full container  overflow-x-hidden h-150 overflow-y-auto">
-
-      <div className="text-white ">
-        {result}
-      </div>
-
-
-
-      </div>
-
-      <div className="bg-gray-900 w-1/2 p-5 pr-5 text-white border border-blue-700 rounded-lg m-auto flex" >
-        <input className="w-full h-full p-2 outline-none" value = {question} onChange={(event)=> setQuestion(event.target.value)} type="text" placeholder="Ask me anything" />
-        <button className="px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-800 transition" onClick={askQuestion } >Enter</button>
-      </div>
-
     </div>
-
-
-
-
-    </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Answers.jsx
@@ -0,0 +1,14 @@
+function Answers({ans,key}){
+
+    console.log(ans,key);
+
+    return(
+        <div>
+            <h1>
+                {ans}
+            </h1>
+        </div>
+    )
+}
+
+export default Answers;
